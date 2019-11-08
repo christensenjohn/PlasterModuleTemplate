31d277be6ca28bdbe0184d4913e0704b16779250
christensenjohn <christensenjohn@hotmail.com>
2019-11-08 10:58:33 +0100

    new functionTemplate

--- ModuleTemplate/functions/FunctionTemplate.ps1
+++ ModuleTemplate/functions/FunctionTemplate.ps1
@@ -14,8 +14,25 @@
 
 function FunctionTemplate {
     [CmdletBinding()]
+    #[OutputType([System.Management.Automation.PSObject])]
     Param (
         $Template
-    )
+    )    
+    $CallerErrorActionPreference = $ErrorActionPreference
+    try {
+        1/0; write-host "will this run" -ForegroundColor Cyan
+<#
+        $PSObject = [PSCustomObject] @{
+            DownloadFileName     = $DownloadFileName
+            DownloadFileNamePath = "$destinationPath\$DownloadFileName"
+            DownloadFileStatus =  $DownloadFileStatus
+        }
+        Write-Output -InputObject $PSObject
+#> 
+    } catch {
+        Write-Error -ErrorRecord $_ -ErrorAction $ErrorActionPreference
+        #FunctionTemplate -ErrorAction Stop
+
+    }
     return $null;
 }
